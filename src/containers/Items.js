7db34abb317a84aa0fdd44ae713f3709950d83d3
nothingcosmos <nothingcosmos@gmail.com>
2018-08-02 21:18:40 +0900

    commitもれ

---
+++ src/containers/Items.js
@@ -0,0 +1,37 @@
+// @flow
+import * as React from 'react';
+import { inject, observer } from 'mobx-react';
+import type { GlobalStore } from '../stores';
+import Home from '../components/Home';
+import TokenCard from '../components/TokenCard';
+
+type Props = {
+  store: GlobalStore,
+};
+
+type State = {
+};
+export default inject('store')(
+  observer(
+    class extends React.Component<Props, State> {
+      componentDidMount() {
+        this.props.store.reloadItems();
+      }
+      render() {
+        return (
+          <Home>
+            {this.props.store.tokenCards.map(tokenCard => (
+              <TokenCard
+                key={tokenCard.tokenId}
+                name={tokenCard.name}
+                createdAt={tokenCard.createdAt}
+                image={tokenCard.image}
+                onClick={() => this.props.store.router.openTokenPageById(tokenCard.tokenId)}
+              />
+            ))}
+          </Home>
+        )
+      }
+    }
+  )
+)
